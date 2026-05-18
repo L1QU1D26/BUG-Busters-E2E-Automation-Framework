9450579f7defd53c1358ac41fc8dbee35ff50f8d
Anand Srivastav <99anandsri@gmail.com>
2026-05-18 15:21:57 +0530

    restructured authentication

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,15 +1,21 @@
-from pages.base_page import BasePage
+from selenium.webdriver.common.by import By
+from test_pom.base_page import BasePage
 
 class LoginPage(BasePage):
 
     def __init__(self, driver):
         super().__init__(driver)
 
-        self.username = ("id", "username")
-        self.password = ("id", "password")
-        self.login_btn = ("id", "login")
+        self.url = "https://example.com/login"
 
-    def login(self, user, pwd):
-        self.enter_text(self.username, user)
-        self.enter_text(self.password, pwd)
-        self.click(self.login_btn)
+        self.username_input = (By.ID, "user-name")
+        self.password_input = (By.ID, "password")
+        self.login_button = (By.ID, "login-button")
+
+    def open_login_page(self):
+        self.open_url(self.url)
+
+    def login(self, username, password):
+        self.enter_text(self.username_input, username)
+        self.enter_text(self.password_input, password)
+        self.click(self.login_button)
