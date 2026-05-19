63d958b5638cc65a33026222c44fceb550e29cfb
anand <abc@gmail.com>
2026-05-19 14:43:10 +0530

    product_page modified

--- pages/product_page.py
+++ pages/product_page.py
@@ -1,11 +1,11 @@
 from selenium.webdriver.common.by import By
-from base_page import BasePage
+from pages.base_page import BasePage
 
 
 class ProductPage(BasePage):
 
     def __init__(self, driver):
-        super()._init_(driver)
+        super().__init__(driver)
 
         self.first_product = (By.CSS_SELECTOR, "a[href='product.php?id=21']")
         self.add_to_cart = (By.CSS_SELECTOR, ".addToCart")
