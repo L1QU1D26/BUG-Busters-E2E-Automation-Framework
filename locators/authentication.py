dd3475125f911c4af00756aa9da85fefe8bb1970
siddartha <siddartha@gmail.com>
2026-05-20 12:56:38 +0530

    authentication is successfully done

---
+++ locators/authentication.py
@@ -0,0 +1,14 @@
+from selenium.webdriver.common.by import By
+
+class LoginLocators:
+
+    USERNAME =(By.ID,"email")
+    PASSWORD= (By.ID,"password")
+    LOGIN_BUTTON= (By.ID,"loginBtn")
+    PRODUCT_TITLE = (By.XPATH,"//a[text()='Shop']")
+    ERROR_MESSAGE = (By.ID,"errorMsg") 
+    EMPTY_EMAIL = (By.ID,"emailerror") 
+    EMPTY_PASS = (By.ID,"passerror")
+    LOGOUT_BUTTON = (By.ID,"logoutBtn")
+
+     
