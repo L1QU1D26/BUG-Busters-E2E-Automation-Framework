ffc6bb87ee1c03a205012c698407e5e7bb3359a9
anand <abc@gmail.com>
2026-05-20 12:06:09 +0530

    Modified navigation and product step definitions

--- step_definitions/navigation_steps.py
+++ step_definitions/navigation_steps.py
@@ -1,13 +1,40 @@
+from pytest_bdd import scenarios, given, when, then
 from pages.navigation_page import NavigationPage
 
 
-class TestNavigation:
+scenarios("../features/navigate.feature")
 
-    def test_navigation_to_products(self, driver):
 
-        navigation = NavigationPage(driver)
+@given("the user is on the homepage")
+def open_homepage(driver):
+    driver.get("https://shop.qaatomationlabs.com/")
 
-        navigation.click_products_menu()
-        navigation.click_view_all_products()
 
-        assert navigation.is_products_page_displayed()
+@when("the user clicks on Products menu")
+def click_products_menu(driver):
+    navigation = NavigationPage(driver)
+    navigation.click_products_menu()
+    navigation.click_view_all_products()
+
+
+@then("the products page should be displayed")
+def validate_products_page(driver):
+    navigation = NavigationPage(driver)
+
+    assert navigation.is_products_page_displayed()
+
+
+@given("the user is logged in")
+def user_logged_in(driver):
+    driver.get("https://shop.qaatomationlabs.com/")
+
+
+@when("the user clicks on Cart icon")
+def click_cart_icon(driver):
+    navigation = NavigationPage(driver)
+    navigation.click_cart_icon()
+
+
+@then("the cart page should open")
+def validate_cart_page(driver):
+    assert "cart" in driver.current_url.lower()
--- step_definitions/product_steps.py
+++ step_definitions/product_steps.py
@@ -1,13 +1,41 @@
+from pytest_bdd import scenarios, given, when, then
 from pages.product_page import ProductPage
 
 
-class TestProduct:
+scenarios("../features/product.feature")
 
-    def test_add_product_to_cart(self, driver):
 
-        product = ProductPage(driver)
+@given("the user is on the products page")
+def open_products_page(driver):
+    driver.get("https://shop.qaatomationlabs.com/")
 
-        product.click_product()
-        product.click_add_to_cart()
 
-        assert product.get_cart_count() == "1"
+@when("the user clicks on a product")
+def click_product(driver):
+    product = ProductPage(driver)
+    product.click_product()
+
+
+@then("product details should be displayed")
+def validate_product_details(driver):
+    assert "product" in driver.current_url.lower()
+
+
+@given("the user is viewing a product")
+def viewing_product(driver):
+    driver.get("https://shop.qaatomationlabs.com/")
+
+
+@when("the user clicks Add to Cart")
+def click_add_to_cart(driver):
+    product = ProductPage(driver)
+
+    product.click_product()
+    product.click_add_to_cart()
+
+
+@then("the product should be added to the cart")
+def validate_cart_count(driver):
+    product = ProductPage(driver)
+
+    assert product.get_cart_count() == "1"
