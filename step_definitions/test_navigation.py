12326dc50b65a4bd9611f4dbf6a948957c2a2390
anand <abc@gmail.com>
2026-05-21 11:22:43 +0530

    New step definition navigation added

--- step_definitions/test_navigation.py
+++ step_definitions/test_navigation.py
@@ -1,34 +1,56 @@
-from selenium.webdriver.common.by import By
-from pages.base_page import BasePage
+from pytest_bdd import given, scenarios, then, when
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+from pages.login_page import LoginPage
+from pages.navigation_page import NavigationPage
 
 
-class NavigationPage(BasePage):
+scenarios("../features/navigate.feature")
 
-    def __init__(self, driver):
-        super().__init__(driver)
 
-        self.products_menu = (
-            By.XPATH,
-            "//a[contains(text(),'Products')]"
-        )
+@given("the user is on the homepage")
+def open_homepage(driver):
+    login = LoginPage(driver)
 
-        self.view_all_products = (
-            By.XPATH,
-            "//a[contains(@href,'shop')]"
-        )
+    login.open_url()
+    login.login_site("demo@demo.com", "demo")
 
-        self.product_page_header = (
-            By.XPATH,
-            "//h2[contains(text(),'Products')]"
-        )
 
-    def click_products_menu(self):
-        self.wait_until_clickable(self.products_menu)
-        self.click(self.products_menu)
+@when("the user clicks on Products menu")
+def click_products_menu(driver):
+    navigation = NavigationPage(driver)
 
-    def click_view_all_products(self):
-        self.wait_until_clickable(self.view_all_products)
-        self.click(self.view_all_products)
+    navigation.click_products_menu()
+    navigation.click_view_all_products()
 
-    def is_products_page_displayed(self):
-        return self.is_displayed(self.product_page_header)
+
+@then("the products page should be displayed")
+def verify_products_page(driver):
+    navigation = NavigationPage(driver)
+
+    assert navigation.is_products_page_displayed()
+
+
+@given("the user is logged in")
+def login_user(driver):
+    login = LoginPage(driver)
+
+    login.open_url()
+    login.login_site("demo@demo.com", "demo")
+
+    assert login.is_dashboard_displayed()
+
+
+@when("the user clicks on Cart icon")
+def click_cart_icon(driver):
+    navigation = NavigationPage(driver)
+
+    navigation.click_cart_icon()
+
+
+@then("the cart page should open")
+def verify_cart_page(driver):
+    assert WebDriverWait(driver, 8).until(
+        EC.url_contains("cart.php")
+    )
